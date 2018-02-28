154a3e85a1a39b1c865d9d7e9eee13e83de45b97
LucasVaz97 <lucastv@al.insper.edu.br>
2018-02-28 04:58:03 -0300

    codigo certo

--- LucasVaz/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
+++ LucasVaz/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
@@ -14,13 +14,33 @@ public class Main {
 		FileReader fr;
 		BufferedReader br;
 		
+		char row;
+		String col;
+		
+	
 		try {
 			fr = new FileReader("C://Users//Lucas Tarraf Vaz//workspace//Java-3-semestre//LucasVaz//src//lab.txt");
 			br = new BufferedReader(fr);
 			String lineRead = br.readLine();
+			
+			row=lineRead.charAt(0);
+			//System.out.println("numero de linhas  "+row);
+			col=lineRead.substring(2,4);
+			//System.out.println("numero de colunas  "+col);	
+			
+			
 			while(lineRead != null){
-				lineRead = lineRead.replace('#','X');
-				System.out.println(lineRead);
+				String certo="";
+				for(int contador = 0; contador < lineRead.length(); contador++) {
+					if(lineRead.charAt(contador)=='#'){
+						certo=certo+"X";
+						}
+					else{
+						certo=certo+" ";
+					}
+				
+				}
+				System.out.println(certo);
 				lineRead = br.readLine();
 			}
 		} catch (IOException e) {
